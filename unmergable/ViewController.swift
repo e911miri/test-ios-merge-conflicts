baecf43fd43df1b771c0a899067dc2fea8f63031
Tomiwa Ijaware <Tomiwa_Ijaware@mckinsey.com>
2018-03-13 19:55:57 +0400

    Initial Commit

---
+++ unmergable/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  unmergable
+//
+//  Created by Tomiwa Ijaware on 13/03/2018.
+//  Copyright © 2018 Tomiwa Ijaware. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
